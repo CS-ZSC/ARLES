6965590cd20faa5ec76f46ff9ca0b88e9213f02b
Mohamed Anwar <mohamed_anwar@opmbx.org>
2016-11-27 17:54:48 +0200

    Added Serial write method

--- Serial.cs
+++ Serial.cs
@@ -13,7 +13,37 @@ namespace ARLES
 
 		public static void write(string port, byte[] program)
 		{
-			
+			int baud = 9600;
+			SerialPort serial = new SerialPort (port, baud);
+
+			try {
+				serial.Open();
+
+				/* Give Arduino time to reset */
+				Thread.Sleep(2000);
+
+				byte[] len = new byte[2];
+				len[0] = (byte) 0x1C;	/* Start of transmition marker */
+				len[1] = (byte) program.Length;
+
+				serial.Write(len, 0, 2);
+				serial.Write(program, 0, program.Length);
+
+				bool valid = true;
+
+				for (int i = 0; i < program.Length; ++i) {
+					byte b = (byte) serial.ReadByte();
+
+					if (b != program[i])
+						valid = false;
+				}
+
+				if (!valid) {
+					/* TODO */
+				}
+			} catch {
+				/* TODO */
+			}
 		}
 	}
 }
